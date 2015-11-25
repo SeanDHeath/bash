78741ef9cab1ab20bd561a39f694b7d022dfe208
Sean Heath <seanheath87@gmail.com>
2015-11-25 18:50:26 -0500

    added automatic sourcing of the new bashrc to the setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o git-prompt.sh
 ln -s git-prompt.sh ~/.git-prompt.sh
 ln -s bashrc ~/.bashrc
+source ~/.bashrc
