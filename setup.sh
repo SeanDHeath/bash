5d9c1888d4e43a1531109a50b0b139518cd56802
Sean Heath <seanheath87@gmail.com>
2015-11-26 00:00:45 +0000

    updated setup script to handle symbolic links correctly

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o git-prompt.sh
-ln -s git-prompt.sh ~/.git-prompt.sh
-ln -s bashrc ~/.bashrc
+cd
+ln -s .bash/git-prompt.sh .git-prompt.sh
+ln -s .bash/bashrc .bashrc
 source ~/.bashrc
