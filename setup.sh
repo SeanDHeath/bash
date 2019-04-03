dd26d337d2c50dc213858e7e7d22ec57b8c134a3
Sean Heath <se@nheath.com>
2019-04-03 10:18:33 -0400

    updated install script to rerun without error

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o git-prompt.sh
-cd
-ln -s .bash/git-prompt.sh .git-prompt.sh
-ln -s .bash/bashrc .bashrc
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.bash/git-prompt.sh
+rm ~/.git-prompt.sh
+rm ~/.bashrc
+ln -s ~/.bash/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.bash/bashrc ~/.bashrc
 source ~/.bashrc
