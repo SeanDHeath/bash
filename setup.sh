2ba49600d24755d9c7821b160724f83adb62cb9a
Sean Heath <se@nheath.com>
2020-05-12 13:04:04 -0400

    updated with git-prompt

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.bash/git-prompt.sh
 rm ~/.git-prompt.sh
 rm ~/.bashrc
 ln -s ~/.bash/git-prompt.sh ~/.git-prompt.sh
